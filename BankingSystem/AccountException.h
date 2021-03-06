3d200bae40199a9bba51b460ae08d35fc19a0cb2
KimYewon <56240088+Yewon-dev@users.noreply.github.com>
2021-03-06 23:17:59 +0900

    Add files via upload

---
+++ BankingSystem/AccountException.h
@@ -0,0 +1,19 @@
+//
+//  AccountException.h
+//  BankingSystem
+//
+//  Created by kimyewon on 2020/11/10.
+//
+
+#ifndef AccountException_h
+#define AccountException_h
+
+class MinusException {
+    
+};
+
+class InsuffException {
+    
+};
+
+#endif /* AccountException_h */
